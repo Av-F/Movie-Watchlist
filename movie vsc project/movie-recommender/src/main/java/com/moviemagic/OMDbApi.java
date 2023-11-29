8aefa7569fc534db7d5d38306e59717d2e2b2985
Av-F <124007306+Av-F@users.noreply.github.com>
2023-11-29 13:35:46 -0500

    Update OMDbApi.java
    Additional comments and removal of API key

--- movie vsc project/movie-recommender/src/main/java/com/moviemagic/OMDbApi.java
+++ movie vsc project/movie-recommender/src/main/java/com/moviemagic/OMDbApi.java
@@ -13,36 +13,36 @@ import org.json.JSONObject;
 public class OMDbApi {
     private static HttpURLConnection connection;
     public static void main(String[] args) {
-        System.out.println("   Movie Genre search!   ");
+        //I wanted to create a simple menu 
+        System.out.println("   Movie Genre search!   "); 
         System.out.println("-------------------------");
-        String cont = "y";
-        while (cont.equals("y")) {
+        String cont = "y"; //String that will work as my boolean variable
+        Scanner movieTitle = new Scanner(System.in); //Create a scanner so that the user can put in their movie
+        String key = "REDACTED"; //Put your own API key here, the code on my desktop has my key in replace of REDACTED
+        String Title;
+        while (cont.equals("y")) { //While loop so the user can make multiple searches
         System.out.println("What movie do you want to look up?");
-        Scanner movieTitle = new Scanner(System.in);
-        String key = "4c87bee2";
-        String Title=movieTitle.nextLine();
-        BufferedReader reader;
-        String line;
-        StringBuffer responseContent = new StringBuffer();
+        Title=movieTitle.nextLine(); //Read the user input
+        BufferedReader reader; //Create a reader to read the url
+        String line; // Generate the line we want to use
+        StringBuffer responseContent = new StringBuffer(); //Create a StringBuffer
         //We use try and catch to access the web
         try {
-        URL url = new URL("https://www.omdbapi.com/?t=" + Title + "&apikey=" + key);
-        connection = (HttpURLConnection) url.openConnection();
+        URL url = new URL("https://www.omdbapi.com/?t=" + Title + "&apikey=" + key); //Create a url obj using the API's endpoint
+        connection = (HttpURLConnection) url.openConnection(); 
             //Request setup
             connection.setRequestMethod("GET");
-            connection.setConnectTimeout(5000);
+            connection.setConnectTimeout(5000); //This equates to 5 seconds
             connection.setReadTimeout(5000);
-            int status=connection.getResponseCode();
+            int status=connection.getResponseCode(); //Fetch the status
         //If we get a status of 200, it means we are connected!  
         if (status == 200) {
-            
-        //If we are connected, we follow the else statement, but we could rewrite it such that if status ==200)    
-       
-            reader =new BufferedReader(new InputStreamReader(connection.getInputStream()));
-             while((line=reader.readLine())!=null) {
-                responseContent.append(line);
+        //If we are connected, then we can fetch the movie title and genre
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream())); //Create a reader
+             while((line=reader.readLine())!=null) { //While the line or data is readable...
+                responseContent.append(line); //Add data to responseContent
             }
-            reader.close();
+            reader.close(); //Close the reader 
             }   
             // Otherwise we show an error
             else {
@@ -52,7 +52,7 @@ public class OMDbApi {
             }
             reader.close();
         }  
-        parse(responseContent.toString());
+        parse(responseContent.toString()); //Parse the data
         }
         catch (MalformedURLException e) {
             e.printStackTrace();
@@ -70,10 +70,10 @@ public class OMDbApi {
 System.out.println("Goodbye!");
 }
 public static String parse(String responseBody) {
-    JSONObject movie = new JSONObject(responseBody);
-    String title = movie.getString("Title");
-    String genre = movie.getString("Genre");
-    System.out.println("Movie Title: " + title + "\n"  + 
+    JSONObject movie = new JSONObject(responseBody); //Create a JSONOBject that is made from the responseBody
+    String title = movie.getString("Title"); //Get the tile
+    String genre = movie.getString("Genre"); //Get the genre
+    System.out.println("Movie Title: " + title + "\n"  + //Print out the data 
                         "Genre: "+genre);
     return null; 
 }
